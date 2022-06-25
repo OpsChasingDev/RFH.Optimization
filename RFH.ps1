b062df21ebb49bf1188ec8f80d596a8de50d6cb6
drummermanrob20 <robert@stapleton-family.net>
2022-06-25 09:17:33 -0400

    init notes

---
+++ RFH.ps1
@@ -0,0 +1,9 @@
+<#
+- each machine has the following logic invoked:
+    - checks the registry to collect all currently logged in users
+    - checks each logged in user's libraries
+    - returns a psobject containing members to describe:
+        - ComputerName
+        - User
+        - library path value
+#>
