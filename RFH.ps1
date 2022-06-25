43c34d55ad64a909250fef99d9961eae13c16b10
drummermanrob20 <robert@stapleton-family.net>
2022-06-25 10:52:37 -0400

    variable name changes

--- RFH.ps1
+++ RFH.ps1
@@ -9,23 +9,23 @@ $User_LoggedIn = $User_LoggedIn | ForEach-Object { $_.Split('\')[1] }
 
 # store usernames for each SID found on the computer in a custom object collection for comparison later
 $SID = Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select-Object -ExpandProperty Name
-$Col_SID = @()
+$colUser = @()
 foreach ($s in $SID) {
     $Prof = Get-ItemProperty -Path "REGISTRY::$s" -Name "ProfileImagePath"
     $User = ($Prof.ProfileImagePath.ToString()).Split('\')[-1]
-    $obj = [PSCustomObject]@{
+    $objUser = [PSCustomObject]@{
         UserSID  = $Prof.PSChildName
         UserName = $User
     }
-    if ($obj.UserSID.Length -gt 25 -and $User_LoggedIn -contains $obj.UserSID) {
-        $Col_SID += $obj
+    if ($objUser.UserSID.Length -gt 25 -and $User_LoggedIn -contains $objUser.UserSID) {
+        $colUser += $objUser
     }
 }
 
 # foreach logged in user, return value of the Desktop path
-foreach ($s in $Col_SID) {
+foreach ($obj in $colUser) {
     $DesktopPathSplat = @{
-        Path        = "REGISTRY::HKU\$($s.UserSID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\"
+        Path        = "REGISTRY::HKU\$($obj.UserSID)\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders\"
         Name        = "Desktop"
         ErrorAction = "SilentlyContinue"
     }
@@ -35,6 +35,6 @@ foreach ($s in $Col_SID) {
         Name       = "Desktop"
         Value      = $DesktopPath
     }
-    $s | Add-Member @DesktopMemberSplat
-    Write-Output $s
+    $obj | Add-Member @DesktopMemberSplat
+    Write-Output $obj
 }
