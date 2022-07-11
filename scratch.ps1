a6d9e876b5fcc88a9bfab4a1aea77522cda10f28
drummermanrob20 <robert@stapleton-family.net>
2022-07-11 10:04:54 -0400

    scratch - verbose preference and changes to output

--- scratch.ps1
+++ scratch.ps1
@@ -2,10 +2,16 @@
 # Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select Name
 
 # returns the username (SamAccountName) and corresponding SID for each user profile on the computer
-$ComputerName = 'sl-computer-001', 'sl-computer-002', 'sl-db-01', 'notonline'
+$VerbosePreference = 'Continue'
+$ComputerName = @(
+    'sl-computer-001'
+    'sl-computer-002'
+    'sl-db-01'
+    'notonline'
+)
 $TotalCount = $ComputerName.Count
 $RemainingCount = $TotalCount
-Write-Output $TotalCount
+Write-Output "$TotalCount computers remaining"
 Write-Progress -Activity "Checking user library paths..." -Status "Running..." -PercentComplete ((0 / $TotalCount) * 100)
 
 Invoke-Command $ComputerName -ErrorAction SilentlyContinue -AsJob {
@@ -19,7 +25,7 @@ do {
     foreach ($j in (Get-Job -IncludeChildJob | Where-Object { $_.State -eq "Completed" -or $_.State -eq "Failed" -and $_.HasMoreData -eq $true })) {
         Receive-Job -Job $j
         $RemainingCount -= 1
-        Write-Output $RemainingCount
+        Write-Output "$RemainingCount computers remaining"
         Write-Progress -Activity "Checking user library paths..." -Status "Running..." -PercentComplete (($RemainingCount / $TotalCount) * 100)
         Write-Verbose "Done checking $($j.Location)"
     }
