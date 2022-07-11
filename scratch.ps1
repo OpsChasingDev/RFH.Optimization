b85cb4bd2fd542df0f6c74fb344fc9316716f2d0
drummermanrob20 <robert@stapleton-family.net>
2022-07-11 09:55:24 -0400

    working progress bar

--- scratch.ps1
+++ scratch.ps1
@@ -2,9 +2,11 @@
 # Get-ChildItem 'REGISTRY::HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\' | Select Name
 
 # returns the username (SamAccountName) and corresponding SID for each user profile on the computer
-$ComputerName = 'sl-computer-001','sl-computer-002','sl-db-01','notonline'
+$ComputerName = 'sl-computer-001', 'sl-computer-002', 'sl-db-01', 'notonline'
 $TotalCount = $ComputerName.Count
+$RemainingCount = $TotalCount
 Write-Output $TotalCount
+Write-Progress -Activity "Checking user library paths..." -Status "Running..." -PercentComplete ((0 / $TotalCount) * 100)
 
 Invoke-Command $ComputerName -ErrorAction SilentlyContinue -AsJob {
     Start-Sleep -Seconds 3
@@ -16,8 +18,9 @@ do {
     # receive the job and update the counter
     foreach ($j in (Get-Job -IncludeChildJob | Where-Object { $_.State -eq "Completed" -or $_.State -eq "Failed" -and $_.HasMoreData -eq $true })) {
         Receive-Job -Job $j
-        $TotalCount -= 1
-        Write-Output $TotalCount
+        $RemainingCount -= 1
+        Write-Output $RemainingCount
+        Write-Progress -Activity "Checking user library paths..." -Status "Running..." -PercentComplete (($RemainingCount / $TotalCount) * 100)
         Write-Verbose "Done checking $($j.Location)"
     }
 } while (
